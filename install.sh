3c60fca35612365a6e4c42120a8dda098c23994d
郑永生 <56060960+Shine6Z@users.noreply.github.com>
2020-04-27 14:14:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt update 
 sudo apt upgrade
-sudo apt install qt5-default qtcreator python3-pyqt5-dbg libqt5svg5 python3-pyqt5.qtsvg qtsvg5-doc
+sudo apt install qt5-default qtcreator python3-pyqt5-dbg libqt5svg5 libqt5svg5-dev python3-pyqt5.qtsvg qtsvg5-doc
 
 cd /home/pi
 git clone https://github.com/bigtreetech/OctoBTT.git
