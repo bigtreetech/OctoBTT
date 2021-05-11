0109826287d0fbb213f54a57870bb2a3ba650820
BigTreeTech <38851044+bigtreetech@users.noreply.github.com>
2021-05-11 09:43:57 +0800

    Update install.sh
    Add Check GLdriver

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ echo "Done."
 #install Dependencies and Display Driver
 echo "Installing Dependencies ..."
 sudo apt install qt5-default qtcreator python3-pyqt5-dbg libqt5svg5 libqt5svg5-dev python3-pyqt5.qtsvg qtsvg5-doc libqt5websockets5 libqt5websockets5-dev ttf-wqy-zenhei qml-module-qtquick-controls2 libboost1.58-all-dev libudev-dev libinput-dev libts-dev libmtdev-dev libjpeg-dev libfontconfig1-dev libssl-dev libdbus-1-dev libglib2.0-dev libxkbcommon-dev libgbm-dev libgl1-mesa-dri qtdeclarative5-dev libqt5svg5 libqt5websockets5 ttf-wqy-zenhei -y
+echo "Check GLdriver ..."
+for package in gldriver-test libgl1-mesa-dri; do
+    if [ "$(dpkg -l "$package" 2> /dev/null | tail -n 1 | cut -d ' ' -f 1)" != "ii" ]; then
+      missing_packages="$package $missing_packages"
+    fi
+done
+apt-get install $missing_packages -y
 echo "Done."
 
 echo "Set render ..."
@@ -61,7 +68,8 @@ EOF
 
 echo "Setting up Autostart ..."
 sudo sed -i 's/sudo \/bin\/sh \/home\/pi\/install.sh/su -l pi -c \"cd \/home\/pi\/OctoBTT \&\& .\/OctoBTT -platform eglfs\"/g' /etc/rc.local
-sudo sed -i '/\[pi4\]/ i \[pi3\]\ndtoverlay=vc4-fkms-v3d' /boot/config.txt
+#sudo sed -i '/\[pi4\]/ i \[pi3\]\ndtoverlay=vc4-fkms-v3d' /boot/config.txt
+sudo sed '/\[all\]/adtoverlay=vc4-fkms-v3d' /boot/config.txt
 echo "Done."
 
 sudo reboot
